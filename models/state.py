6215e2241366b349ac1b15a255b95d28363383cf
Oma-yma1 <zgani.omayma@gmail.com>
2023-09-19 07:17:09 -0700

    task6

--- models/state.py
+++ models/state.py
@@ -1,8 +1,32 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, Integer, String
+import models
+from models.city import City
+import shlex
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", cascade='all, delete, delete-orphan',
+                          backref="state")
+
+    @property
+    def cities(self):
+        vrp = models.storage.all()
+        liste = []
+        resultat = []
+        for i in vrp:
+            city = i.replace('.', ' ')
+            city = shlex.split(city)
+            if (city[0] == 'City'):
+                liste.append(vrp[i])
+        for element in liste:
+            if (element.state_id == self.id):
+                resultat.append(element)
+        return (resultat)
