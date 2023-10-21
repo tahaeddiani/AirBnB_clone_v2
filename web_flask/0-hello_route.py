2602a7b0293b6486557937c29898cd304d83bc0d
tahaeddiani <tahaeddiani2@gmail.com>
2023-10-21 07:21:07 -0700

    t0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -4,10 +4,12 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def home():
     """home func"""
     return "Hello HBNB!"
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
