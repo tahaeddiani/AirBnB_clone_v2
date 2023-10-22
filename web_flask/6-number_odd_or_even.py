f82a7b40af4fc2f23f523ec72ced62004c1ec997
tahaeddiani <tahaeddiani2@gmail.com>
2023-10-22 08:58:31 -0700

    t6

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -16,13 +16,11 @@ def hbnb():
     """hbnb func"""
     return "HBNB"
 
-
 @app.route('/c/<text>', strict_slashes=False)
 def TextHbnb(text):
     """Text hbnb func"""
     text = text.replace('_', ' ')
     return "C " + text
-
 
 @app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
@@ -31,24 +29,25 @@ def python_text(text):
     text = text.replace("_", " ")
     return "Python {}".format(text)
 
-
 @app.route("/number/<int:n>", strict_slashes=False)
 def number(n):
     """is it a number"""
     return "{} is a number".format(n)
-
 
 @app.route("/number_template/<int:n>", strict_slashes=False)
 def number_template(n):
     """Displays an HTML page """
     return render_template("5-number.html", n=n)
 
-
-@app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
 def number_odd_or_even(n):
-    """t6
-    """
-    return render_template("6-number_odd_or_even.html", n=n)
+    """t6 """
+    if n % 2 == 0:
+        even_or_odd = "even"
+    else:
+        even_or_odd = "odd"
+    return render_template(
+        '6-number_odd_or_even.html', n=n, even_or_odd=even_or_odd)
 
 
 if __name__ == "__main__":
