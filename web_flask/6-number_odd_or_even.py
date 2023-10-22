4eb2956071ee88fdea8fc94a23a5c939226e231e
tahaeddiani <tahaeddiani2@gmail.com>
2023-10-22 08:59:50 -0700

    t6

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -16,11 +16,13 @@ def hbnb():
     """hbnb func"""
     return "HBNB"
 
+
 @app.route('/c/<text>', strict_slashes=False)
 def TextHbnb(text):
     """Text hbnb func"""
     text = text.replace('_', ' ')
     return "C " + text
+
 
 @app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
@@ -29,15 +31,18 @@ def python_text(text):
     text = text.replace("_", " ")
     return "Python {}".format(text)
 
+
 @app.route("/number/<int:n>", strict_slashes=False)
 def number(n):
     """is it a number"""
     return "{} is a number".format(n)
 
+
 @app.route("/number_template/<int:n>", strict_slashes=False)
 def number_template(n):
     """Displays an HTML page """
     return render_template("5-number.html", n=n)
+
 
 @app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
 def number_odd_or_even(n):
