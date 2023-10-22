8fbf53262110a3f519689172a9324eebbf721c78
tahaeddiani <tahaeddiani2@gmail.com>
2023-10-22 08:23:34 -0700

    t5

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""t5"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def home():
+    """home func"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """hbnb func"""
+    return "HBNB"
+
+@app.route('/c/<text>', strict_slashes=False)
+def TextHbnb(text):
+    """Text hbnb func"""
+    text = text.replace('_', ' ')
+    return "C " + text
+
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_text(text):
+    """Text hbnb func"""
+    text = text.replace("_", " ")
+    return "Python {}".format(text)
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def number(n):
+    """is it a number"""
+    return "{} is a number".format(n)
+
+@app.route("/number_template/<int:n>", strict_slashes=False)
+def number_template(n):
+    """Displays an HTML page """
+    return render_template("5-number.html", n=n)
+
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
